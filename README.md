a0abfba4caa5ffe5a4f9fc3d44c8a30d35f1d5a5
Ivan Vučica <ivan@vucica.net>
2015-09-06 19:22:21 +0100

    Initial functionality.

---
+++ README.md
@@ -0,0 +1,29 @@
+# IceWM desktop file extractor
+
+Generates entries parseable by IceWM's menuprog command.
+
+Parses [FreeDesktop.org .desktop and .directory][3] entries, trying to
+have a very basic compliance with the [menu][2] spec. Uses [pyxdg][1]
+for parsing.
+
+## Usage
+
+Use by adding the following to e.g. `${HOME}/.icewm/menu`, or perhaps to
+`/etc/X11/icewm/menu` or maybe `/usr/share/icewm/menu`:
+
+    menuprog "FD.o Programs" folder /path/to/icewm-desktop-files/icewm-desktop-files.py
+
+You may also be interested in `icewm-menu-gnome2` from the [icewm-gnome-support][4] package, which does a similar thing.
+
+> Copyright (c) 2015 Ivan Vucica
+> 
+> Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+> 
+> The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+> 
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+[1]: http://pyxdg.readthedocs.org/en/latest/desktopentry.html
+[2]: http://standards.freedesktop.org/menu-spec/latest
+[3]: http://standards.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+[4]: apt://icewm-gnome-support
---
+++ icewm-desktop-files.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+import os
+import os.path
+import sys
+
+import xdg.BaseDirectory
+import xdg.DesktopEntry
+from operator import methodcaller
+
+def all_desktop_files(path=None):
+    if not path:
+        for path in xdg.BaseDirectory.xdg_data_dirs:
+            for app in all_desktop_files(path):
+                yield app
+        return
+
+    path = os.path.join(path, 'applications')
+    if not os.path.exists(path):
+        return
+
+    all_files = [f for f in os.listdir(path)
+                 if os.path.isfile(os.path.join(path, f))]
+    desktop_files = [f for f in all_files if f.endswith('.desktop')]
+
+    for desktop_file in desktop_files:
+        yield os.path.join(path, desktop_file)
+
+def all_apps():
+    for desktop_file in all_desktop_files():
+        desktop_entry = xdg.DesktopEntry.DesktopEntry(desktop_file)
+        if desktop_entry.getType() != 'Application':
+            continue
+        yield desktop_entry
+
+
+def all_desktop_directory_files(path=None):
+    if not path:
+        for path in xdg.BaseDirectory.xdg_data_dirs:
+            for app in all_desktop_directory_files(path):
+                yield app
+        return
+
+    path = os.path.join(path, 'desktop-directories')
+    if not os.path.exists(path):
+        return
+
+    all_files = [f for f in os.listdir(path)
+                 if os.path.isfile(os.path.join(path, f))]
+    directory_files = [f for f in all_files if f.endswith('.directory')]
+
+    for directory_file in directory_files:
+        yield os.path.join(path, directory_file)
+
+
+def all_desktop_directories():
+    for directory_file in all_desktop_directory_files():
+        directory_entry = xdg.DesktopEntry.DesktopEntry(directory_file)
+        if directory_entry.getType() != 'Directory':
+            continue
+        directory_entry.path = directory_file
+        yield directory_entry
+
+def app_in_directory(app_entry, dir_entry):
+    category = dir_entry.getName()
+    return app_in_category(app_entry, category)
+
+def app_in_category(app_entry, category):
+    app = app_entry
+    # TODO(ivucica): Verify notshowin does what I thought it does.
+    if app.getNotShowIn() and category in app.getNotShowIn():
+        return False
+    if app.getOnlyShowIn() and 'Old' not in app.getOnlyShowIn():
+        return False
+    if app.getCategories() and category in app.getCategories():
+        return True
+    return False
+
+def apps_in_directory(app_entries, dir_entry):
+    for app in app_entries:
+        if app_in_directory(app, dir_entry):
+            yield app
+
+def apps_in_category(app_entries, category):
+    for app in app_entries:
+        if app_in_category(app, category):
+            yield app
+
+
+def list_all_apps():
+    for app in all_apps():
+        print unicode(app).encode('utf-8')
+def list_all_dirs():
+    for dr in all_desktop_directories():
+        print unicode(dr).encode('utf-8')
+
+def list_apps_per_dir():
+    all_apps_list = list(all_apps())
+    for dr in all_desktop_directories():
+        apps = list(apps_in_directory(all_apps_list, dr))
+        if len(apps):
+            print '[%s]' % dr.getName()
+            print [app.getName() for app in apps]
+
+def icewm_dirs():
+    all_apps_list = list(all_apps())
+
+    categories = ['AudioVideo', 'Audio', 'Video', 'Development', 'Education', 'Game', 'Graphics', 'Network', 'Office', 'Settings', 'System', 'Utility']
+    for category in categories:
+        apps = list(apps_in_category(all_apps_list, category))
+	if len(apps):
+            print 'menuprog "%s" folder "%s" "%s"' % (category, os.path.abspath(sys.argv[0]), category)
+
+    all_desktop_directories_list = list(all_desktop_directories())
+    list.sort(all_desktop_directories_list, key=methodcaller('getName'))
+
+    for dr in all_desktop_directories_list:
+        apps = list(apps_in_directory(all_apps_list, dr))
+	if len(apps):
+            print 'menuprog "%s" folder "%s" "%s"' % (dr.getName(), os.path.abspath(sys.argv[0]), dr.path)
+
+def icewm_apps(directory_file_or_category=None):
+    all_apps_list = list(all_apps())
+    if directory_file_or_category[0] == '/':
+	directory_entry = xdg.DesktopEntry.DesktopEntry(directory_file_or_category)
+	apps = list(apps_in_directory(all_apps_list, directory_entry))
+    else:
+	apps = list(apps_in_category(all_apps_list, directory_file_or_category))
+    
+    list.sort(apps, key=methodcaller('getName'))
+    for app in apps:
+        print 'prog "%s" "%s" %s' % (app.getName(), app.getIcon(), app.getExec())
+
+if len(sys.argv) > 1:
+    icewm_apps(sys.argv[1])
+else:
+    icewm_dirs()
